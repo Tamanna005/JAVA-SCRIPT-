c2189d362ae8b44e4bc8def5773551d9ba9eee46
Tamanna Garva <155551840+Tamanna005@users.noreply.github.com>
2024-02-06 15:30:07 +0530

    Add files via upload

---
+++ Grades.js
@@ -0,0 +1,11 @@
+let score = 100;
+let grade;
+core >=89 && score <=70){
+    grade = "c";
+    }else if(score >=79 && score <=60){
+    grade = "d";
+    }else if(score >=69 && score <=50){
+    grade = "e";
+    }else if(score >=49 && score <=0){
+        grade = "f";
+    }
