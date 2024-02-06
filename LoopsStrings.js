f68d7cf4ce7271fa7dac85a13dbde9736cedfea8
Tamanna Garva <155551840+Tamanna005@users.noreply.github.com>
2024-02-06 15:31:17 +0530

    Add files via upload

---
+++ LoopsStrings.js
@@ -0,0 +1,110 @@
+//FOR LOOP
+
+// for(let count=1; count<=5; count++){
+//     console.log("count =", count);//s execute
+// }
+
+//SUM
+
+// Calculate sum of 1 to 5 
+// let sum = 0;
+// for (let i = 1; i<=n; i++){
+//     sum = sum + i; //sum = 1
+// }
+// console.log("sum =", sum);
+
+// WHILE LOOP
+
+// while(condition){
+//     // do some work
+// }
+
+// let i=1 ;
+// while(i<=5){
+//     console.log("i =", i);
+//     i++;
+// }
+
+//DO WHILE LOOP
+// let i=1 ;
+// do(i<=5){
+//     console.log("i =", i);
+//     i++;
+// } while()
+
+// Practice quition
+//Q.1 Print all even numbers from 0 to 100.
+// for(let num=0; num<=100; num++){
+//     if(num%2===0){  //even number
+//         console.log("num=", num);
+//     }
+//     if(num%2!==0){//odd number
+//         console.log("num=", num);
+//     }
+// };
+
+
+// for-of Loop
+// for(let val of strVar){
+//     //do some work
+// }
+
+// let str="apanacollege";
+
+// let size = 0;
+// for(let i of str){
+//     console.log("i=", i);
+//     size++;
+// }
+
+// console.log("string size =", size);
+
+// for in loop
+// for(let key in objVar){
+//     // do some work
+// }
+// let student ={
+//     name:"Rhahulkumar",
+//     age:20,
+//     cgpa:7.5,
+//     isPass:true,
+// };
+// for (let key in student){
+//     console.log("key=", key, "value=",  student[key]);
+// }
+
+//Practice Q.2
+// let gameNum = "25";
+
+// let userNum = prompt( "guess the game number : ");
+
+// while(userNum != gameNum){
+//     userName = prompt("You entered wrong number. Guess again:");
+// }
+// console.log("congratulations, you enterd th right number");
+
+
+// create String 
+// let str="Jignesh";
+//Strings
+// let str = "jignesh";
+// let str3 = 'jignesh';
+
+// String length 
+// str.length
+
+// String Indices
+// str[0],str[1],str[2]
+// console.log(string[1]);
+
+//Template Literals
+// let specialString=This is a template literal;
+// console.log(specialString);
+
+// let obj = {
+//     item : "pen", 
+//     price:10,
+// };
+// let output = the cast of ${obj.item} is ${obj.price} rupees;
+// console.log(output);
+// console.log("the cost of", obj.item, "is", obj.price, "rupess");
