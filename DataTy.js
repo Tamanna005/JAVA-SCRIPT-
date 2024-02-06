1fddec69349c99ed460ba1d1074752c7f09a2da3
Tamanna Garva <155551840+Tamanna005@users.noreply.github.com>
2024-02-06 15:30:39 +0530

    Add files via upload

---
+++ DataTy.js
@@ -0,0 +1,49 @@
+let length = 16; //number
+let lastName = "Brown"; //string
+
+let x={
+    firstName:"Nick",
+    lastName:"Stark"
+}; //object
+
+//Q2.
+let y=50;
+
+//Q3.
+let fruitName = 50;
+let Orange = null;
+
+//Q4.
+x = 3;
+y = 10;
+
+console.log(x + y);
+
+//Q5.
+let a = 3;
+let b = 7;
+let z = a+b;
+alert(z);
+
+//Q6.
+
+let firstName = "David";
+let LastName = "johnson";
+let age = 41;
+
+//Q7.
+
+const Nescafe ={
+    productName :"Nascafe Coffee",
+    price : 300,
+    madeIn : "russia",
+    Packaging : "Glass"
+};
+
+//Q8.
+let Q = 13;
+if(13/7){
+    alert(Q);
+}else{
+    alert("error!");
+}
